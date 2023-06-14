d721b9480d9539e5453233746796f37721da3273
MaciejPel <maciejpelczynski0@gmail.com>
2023-06-15 01:32:35 +0200

    add message for external users about verification on register page

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,18 +1,18 @@
 interface ContainerProps {
-	children: React.ReactNode;
-	type: 'center' | 'start';
+  children: React.ReactNode;
+  type: "center" | "start";
 }
 
 const Container: React.FC<ContainerProps> = ({ children, type }) => {
-	return (
-		<main
-			className={`px-4 lg:px-8 flex flex-1 flex-col items-center lg:w-4/5 w-full ${
-				type === 'center' ? 'justify-center pb-20' : 'justify-start'
-			}`}
-		>
-			{children}
-		</main>
-	);
+  return (
+    <main
+      className={`px-4 lg:px-8 flex flex-1 flex-col items-center lg:w-4/5 w-full ${
+        type === "center" ? "justify-center" : "justify-start"
+      }`}
+    >
+      {children}
+    </main>
+  );
 };
 
 export default Container;
--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,14 +1,14 @@
 // src/server/db/client.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-	var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
-	global.prisma ||
-	new PrismaClient({
-		// log: ['query', 'info', 'warn', 'error'],
-	});
+  global.prisma ||
+  new PrismaClient({
+    // log: ['query', 'info', 'warn', 'error'],
+  });
 
 global.prisma = prisma;
